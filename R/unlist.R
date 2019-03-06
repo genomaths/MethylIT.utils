0f6c5687be83d04d91a008cf4926d3df4edaa711
genomaths <18340747+genomaths@users.noreply.github.com>
2019-03-06 18:57:45 -0500

    Update doc for function unlist

--- R/unlist.R
+++ R/unlist.R
@@ -1,17 +1,15 @@
 #' @rdname unlist
-#' @aliases unlist.pDMP
-#' @aliases unlist.InfDiv
 #' @title Flatten Lists extended to 'pDMP' and 'InfDiv' classes
-#' @description Given a 'pDMP' or 'InfDiv' objects, unlist simplifies it to
-#'     produce a GRanges object which contains all the GRanges components which
-#'     occur in 'pDMP' or 'InfDiv' object.
+#' @description Given a list 'x' of R objects from the same class and same
+#'     format, unlist simplifies it to produce a new R object which contains all
+#'     the initial components which in 'x' object.
 #' @param x Any list R object.
 #' @details This is a method to extend unlist generic function to handle
 #'     any list of objects from the same class.
 #' @export
 #' @examples
 #' gr1 <-GRanges(seqnames = "chr2", ranges = IRanges(3, 6),
-#'           strand = "+", score = 5L, GC = 0.45)
+#'               strand = "+", score = 5L, GC = 0.45)
 #' gr2 <-
 #'   GRanges(seqnames = c("chr1", "chr1"),
 #'           ranges = IRanges(c(7,13), width = 3),
@@ -21,9 +19,8 @@
 #'           ranges = IRanges(c(1, 4), c(3, 9)),
 #'           strand = c("-", "-"), score = c(6L, 2L), GC = c(0.4, 0.1))
 #' grl <- list("gr1" = gr1, "gr2" = gr2, "gr3" = gr3)
-#' # unlist(grl) # It does not work
-#' class(grl) <-'InfDiv' # A trick
-#' unlist(grl) # It works
+#' base::unlist(grl) # The default unlist does not work
+#' unlist(grl)
 
 setGeneric("unlist", signature = "x")
 unlist <- function(x, ...) UseMethod("unlist", x)
