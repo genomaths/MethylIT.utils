a865082edc8f376ccd89890d8816c09c4b5d350a
genomaths <18340747+genomaths@users.noreply.github.com>
2019-05-05 17:23:21 -0400

    Update function predictDIMPclass

--- R/predictDIMPclass.R
+++ R/predictDIMPclass.R
@@ -2,31 +2,37 @@
 #' @rdname predictDIMPclass
 #' @title Predict DIMP class
 #' @description This function classify each DIMP as a control or a treatment
-#'   DIMP
+#'     DIMP
 #' @details Predictions only makes sense if the query DIMPs belong to same
-#'   methylation context and derive from an experiment accomplished under the
-#'   same condition set for the DIMPs used to build the model.
+#'     methylation context and derive from an experiment accomplished under the
+#'     same condition set for the DIMPs used to build the model.
 #' @param LR A list of GRanges objects obtained through the through MethylIT
-#'   downstream analysis. Basically, this object is a list of GRanges containing
-#'   only differentially methylated position (DMPs). The metacolumn of each
-#'   GRanges must contain the columna: Hellinger divergence "hdiv", total
-#'   variation "TV", the probability of potential DMP "wprob", which naturally
-#'   are added in the downstream analysis of MethylIT.
+#'     downstream analysis. Basically, this object is a list of GRanges containing
+#'     only differentially methylated position (DMPs). The metacolumn of each
+#'     GRanges must contain the columna: Hellinger divergence "hdiv", total
+#'     variation "TV", the probability of potential DMP "wprob", which naturally
+#'     are added in the downstream analysis of MethylIT.
 #' @param model A classifier model obtained with the function
-#' 'evaluateDIMPclass'.
+#'     'evaluateDIMPclass'.
 #' @param conf.matrix Optional. Logic, whether a confusion matrix should be
-#'   returned (default, FALSE, see below).
+#'     returned (default, FALSE, see below).
 #' @param control.names Optional. Names/IDs of the control samples, which must
-#'   be include in thr variable LR (default, NULL).
+#'     be include in thr variable LR (default, NULL).
 #' @param treatment.names Optional. Names/IDs of the treatment samples, which
-#'   must be include in the variable LR (default, NULL).
+#'     must be include in the variable LR (default, NULL).
+#' @param num.cores,tasks Paramaters for parallele computation using package
+#'     \code{\link[BiocParallel]{BiocParallel-package}}: the number of cores to
+#'     use, i.e. at most how many child processes will be run simultaneously
+#'     (see \code{\link[BiocParallel]{bplapply}} and the number of tasks per job
+#'     (only for Linux OS).
 #' @return The same LR object with a column named "class" added to a GRanges
-#'   object from LR (default). Based on the model prediction each DIMP is
-#'   labeled as control "CT" or as treatment "TT". If "conf.matrix" is TRUE and
-#'   the arguments control.names and treatment.names are provided, then the
-#'   overall confusion matrix is returned
+#'     object from LR (default). Based on the model prediction each DIMP is
+#'     labeled as control "CT" or as treatment "TT". If "conf.matrix" is TRUE and
+#'     the arguments control.names and treatment.names are provided, then the
+#'     overall confusion matrix is returned
 #' @importFrom MethylIT unlist
 #' @importFrom caret confusionMatrix
+#' @importFrom BiocParallel MulticoreParam bplapply SnowParam
 #' @examples 
 #' library(MethylIT)
 #' 
@@ -55,77 +61,84 @@
 #'                         treatment.names = c("T1", "T2", "T3"))
 #' @export
 predictDIMPclass <- function(LR, model, conf.matrix = FALSE,
-                             control.names = NULL,
-                             treatment.names = NULL) {
-  if (conf.matrix && (is.null(control.names) || is.null(treatment.names))) {
+                          control.names = NULL, treatment.names = NULL, 
+                          num.cores=1, tasks=0L) {
+   if (conf.matrix && (is.null(control.names) || is.null(treatment.names))) {
        stop(paste0("* if conf.mat = TRUE, then the character vectors for ",
                    "control.names and treatment.names must be provided"))
-  }
+   }
   
-  if (inherits(LR, "GRangesList")) LR <- as(LR, "list")
-  if (class(LR)[1] == "list") class(LR) <- "pDMP"
-  r1 <- try(validateClass(LR), silent = TRUE)
-  if (inherits(r1, "try-error"))
+   if (inherits(LR, "GRangesList")) LR <- as(LR, "list")
+   if (class(LR)[1] == "list") class(LR) <- "pDMP"
+   r1 <- try(validateClass(LR), silent = TRUE)
+   if (inherits(r1, "try-error"))
        stop("*** LR is not an object from 'pDMP' class or is not", 
-            " coercible to 'pDMP' class" )
+           " coercible to 'pDMP' class" )
   
-  if (conf.matrix && !is.null(control.names) && !is.null(treatment.names)) {
-    sn = names(LR)
-    idx.ct = match(control.names, sn)
-    idx.tt = match(treatment.names, sn)
-    CT = LR[ idx.ct ]
-    CT = unlist(CT)
-    TT = LR[ idx.tt ]
-    TT = unlist(TT)
-    classSet = list(CT = CT, TT = TT)
-  } else classSet = LR
+   if (conf.matrix && !is.null(control.names) && !is.null(treatment.names)) {
+       sn = names(LR)
+       idx.ct = match(control.names, sn)
+       idx.tt = match(treatment.names, sn)
+       CT = LR[ idx.ct ]
+       CT = unlist(CT)
+       TT = LR[ idx.tt ]
+       TT = unlist(TT)
+       classSet = list(CT = CT, TT = TT)
+   } else classSet = LR
 
-  position <- function(gr) {
-    chrs = split(gr, seqnames(gr))
-    gr = lapply(chrs, function(grc) {
-      x = start(grc)
-      x.min = min(x)
-      x.max = max(x)
-      delta =  max(c(x.max - x, 1))
-      return((x - x.min)/(delta))
-    })
-    return(unlist(gr))
-  }
+   position <- function(gr) {
+       chrs <- split(gr, seqnames(gr))
+       gr <- lapply(chrs, function(grc) {
+                               x = start(grc)
+                               x.min <- min(x)
+                               x.max <- max(x)
+                               delta <- max(c(x.max - x, 1))
+                               return((x - x.min)/(delta))}
+       )
+       return(unlist(gr))
+   }
 
-  classifier <- function(GR) {
-    cn = colnames(mcols(GR))
-    vn = c("hdiv", "TV", "wprob")
-    vn = cn[is.element(cn, vn)]
-    m = mcols(GR[, vn])
-    m$pos = position(GR)
-    if (is.element("wprob", vn)) {
-      vn = sub("wprob", "logP", vn)
-      colnames(m) <- c(vn, "pos")
-      m$logP = log10(m$logP + 2.2e-308)
-    }
+   classifier <- function(GR) {
+       cn = colnames(mcols(GR))
+       vn = c("hdiv", "TV", "wprob")
+       vn = cn[is.element(cn, vn)]
+       m = mcols(GR[, vn])
+       m$pos = position(GR)
+       if (is.element("wprob", vn)) {
+           vn = sub("wprob", "logP", vn)
+           colnames(m) <- c(vn, "pos")
+           m$logP = log10(m$logP + 2.2e-308)
+       }
 
-    if (inherits(model, "glm")) {
-      model = structure(model, class = c("LogisticR", "glm"))
-    }
-    if (inherits(model, "lda") || inherits(model, "qda")) {
-      pred = predict(model, newdata = m )$class
-    } else pred = predict(model, newdata = m, type = "class")
-    GR$class = pred
-    return(GR)
-  }
-  LR = lapply(classSet, classifier)
-  if (!conf.matrix) {
-    return(LR)
-  } else {
-    TRUE_class <- factor(c(rep("CT", length(CT)), rep("TT", length(TT))), 
+       if (inherits(model, "glm")) {
+           model = structure(model, class = c("LogisticR", "glm"))
+       }
+       if (inherits(model, "lda") || inherits(model, "qda")) {
+           pred <- predict(model, newdata = m )$class
+       } else pred <- predict(model, newdata = m, type = "class")
+           GR$class <- pred
+           return(GR)
+   }
+  
+   if (num.cores > 1) {
+       if (Sys.info()['sysname'] == "Linux") {
+           bpparam <- MulticoreParam(workers = num.cores, tasks=tasks)
+       } else bpparam <- SnowParam(workers = num.cores, type = "SOCK")
+       LR = bplapply(classSet, classifier, BPPARAM = bpparam)
+   } else LR = lapply(classSet, classifier)
+   
+   if (!conf.matrix) {
+       return(LR)
+   } else {
+       TRUE_class <- factor(c(rep("CT", length(CT)), rep("TT", length(TT))), 
                        levels = c("CT", "TT"))
-    PRED_class <- factor(c(as.character(LR$CT$class), 
+       PRED_class <- factor(c(as.character(LR$CT$class), 
                            as.character(LR$TT$class)),
                        levels = c("CT", "TT"))
     
-    conf.mat <- confusionMatrix(data=PRED_class, reference=TRUE_class,
+       conf.mat <- confusionMatrix(data=PRED_class, reference=TRUE_class,
                                positive="TT")
     
-    return(conf.mat = conf.mat)}
+       return(conf.mat = conf.mat)}
 }
 
